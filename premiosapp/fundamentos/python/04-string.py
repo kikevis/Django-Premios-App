4ed90bd9a1f5e7b9ea08de4129890c56712e0d76
kikevis <villasanchezg22@gmail.com>
2023-07-06 17:30:22 -0500

    string

---
+++ premiosapp/fundamentos/python/04-string.py
@@ -0,0 +1,24 @@
+name = "Geovanny"
+last_name = 'Villa'
+print(name)
+print(last_name)
+
+full_name = name + " " + last_name
+print(full_name)
+
+quote = "I'm Geovanny"
+print(quote)
+
+quote2 = ' He said "Hello"  '
+print(quote2)
+
+# format
+template = "Hola, mi nombre es " + name + " y mi apellido es " + last_name
+print('v1 ', template)
+
+template = "Hola, mi nombre es {} y mi apellido es {}".format(name, last_name)
+
+print('v2', template)
+
+template = f"Hola, mi nombre es {name} y mi apellido es {last_name}"
+print('v3', template)
