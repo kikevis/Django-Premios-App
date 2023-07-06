9d6216a00dcc7a76c058e4d6e5542d305fda105c
kikevis <villasanchezg22@gmail.com>
2023-07-06 10:24:15 -0500

    app polls 1.1

--- premiosapp/polls/urls.py
+++ premiosapp/polls/urls.py
@@ -1,17 +1,59 @@
 from django.urls import path
 from . import views
 
+from django.views.generic.dates import ArchiveIndexView
+from polls.models import Question
+
 app_name = "polls"
 urlpatterns = [
     #ex: /polls/
-    path("", views.index, name="index"),
+    # path(
+    #     "",
+    #     views.index,
+    #     name="index"
+    # ),
+
+    #ex: /polls/ ListView
+    path(
+        '',
+        views.ListQuestion.as_view(),
+        name='index'
+    ),
 
     #ex: /polls/1/
-    path("<int:question_id>/detail/", views.detail, name="detail"),
+    path(
+        # "<int:question_id>/detail/",
+        "<int:pk>/detail/",
+        views.DetailView.as_view(),
+        name="detail"
+    ),
 
     #ex: /polls/1/results
-    path("<int:question_id>/results/", views.results, name="results"),
+    path(
+        # "<int:question_id>/results/",
+        "<int:pk>/results/",
+        views.ResultView.as_view(),
+        name="results"
+    ),
 
     #ex: /polls/1/vote
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path(
+        "<int:question_id>/vote/",
+        views.vote,
+        name="vote"
+    ),
+
+    #ex: 2023/jun/8 and 2023/jun/14
+    path(
+        "<int:year>/<str:month>/<int:day>/",
+        views.QuestionDayArchiveView.as_view(),
+        name="question archive day"
+    ),
+
+    #ex: /polls/prueba DayArchiveView
+    path(
+        'prueba/',
+        ArchiveIndexView.as_view(model=Question, date_field="pub_date"),
+        name="article_archive.html"
+    ),
 ]
--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -2,39 +2,68 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
+from django.views.generic.dates import DayArchiveView
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    lastest_question_list = Question.objects.all()
-    return render(request, "polls/index.html", {
-        "latest_question_list": lastest_question_list
-    })
-    # return HttpResponse("Hello World, Polls")
+# def index(request):
+#     lastest_question_list = Question.objects.all()
+#     return render(request, "polls/index.html", {
+#         "latest_question_list": lastest_question_list
+#     })
+#     # return HttpResponse("Hello World, Polls")
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {
-        "question": question
-    })
-    # return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+class ListQuestion(ListView):
+    model = Question
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    # queryset = Question.objects.all()
+    queryset = Question.objects.order_by("pub_date")[:5]
+    # queryset = Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    # return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {
-        "question": question
-    })
+# class ListQuestion(ListView):
+#     template_name = "polls/index.html"
+#     context_object_name = "latest_question_list"
+
+#     def get_queryset(self):
+#         return Question.objects.order_by("-pub_date") [:5]
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {
+#         'question': question
+#     })
+#     # return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+class DetailView(DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+# def results(request, question_id):
+#     # return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {
+#         'question': question
+#     })
+
+class ResultView(DetailView):
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     # return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
-        return render(request, "polls/detail.html", {
-            "question": question,
-            "error_menssage": "No elegiste una respuesta"
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_menssage': 'No elegiste una respuesta'
         })
     else:
-        selected_choice.votes+=1
+        selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class QuestionDayArchiveView(DayArchiveView):
+    queryset = Question.objects.all()
+    date_field = 'pub_date'
+    allow_empty = True
