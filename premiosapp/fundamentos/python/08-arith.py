ef0c833caf3bd6a4aa4353c6989191bfc1be0804
kikevis <villasanchezg22@gmail.com>
2023-07-12 17:34:21 -0500

    fundamentos

--- premiosapp/fundamentos/python/08-arith.py
+++ premiosapp/fundamentos/python/08-arith.py
@@ -2,7 +2,7 @@ print(10 + 10)
 print(10 - 5)
 print(10 * 2)
 print(10 / 2) # 5
-print(10 % 2) # 0
+print(10 % 2) # 0 residuo'
 print(10 / 3)
 print(10 % 3)
 print(10 // 3)
@@ -13,6 +13,14 @@ print((7 / 1) // 4)
 
 print(8 + 3 - 1)
 print(10 / 0)
+
+# (+) Suma
+# (-) Resta
+# (*) Multiplicación
+# (/) División
+# (%) Modulo o Residuo
+# (//) Division con valor entero
+# (**) Exponenciación
 
 print('Hola' + ' mundo')
 print('Hola' * 3)
